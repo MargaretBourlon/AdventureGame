95b9c232b72e28a9cfdfd541621329ebedc12f96
MargaretBourlon <mdbourlon@gmail.com>
2016-05-27 19:39:43 -0500

    added first ending

--- AdventureGame.js
+++ AdventureGame.js
@@ -1,3 +1,7 @@
 function showNext (nextScene) {
   document.getElementById(nextScene).style.display = "block";
 };
+function switchScene (currentScene, alternateScene) {
+    document.getElementById(currentScene).style.display = "none";
+    document.getElementById(alternateScene).style.display = "block";
+};
