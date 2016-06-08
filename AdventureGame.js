54305bb19efee243d191bad61daa30832df72439
MargaretBourlon <mdbourlon@gmail.com>
2016-06-08 16:27:10 -0500

    added gifs and preliminary styling

--- AdventureGame.js
+++ AdventureGame.js
@@ -1,7 +1,4 @@
 function showNext (nextScene) {
   document.getElementById(nextScene).style.display = "block";
-};
-function switchScene (currentScene, alternateScene) {
-    document.getElementById(currentScene).style.display = "none";
-    document.getElementById(alternateScene).style.display = "block";
+  document.getElementById(nextScene).scrollIntoView();
 };
